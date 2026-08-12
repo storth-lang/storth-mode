c5c4b6db21e85c2ab27b5fc4efc3157e9d60164f
segfault <tadihailukebe@gmail.com>
2026-08-12 17:10:57 +0000

    Fixing some stuff

--- storth-mode.el
+++ storth-mode.el
@@ -22,7 +22,7 @@
 
 (defconst storth-keywords
   '("fn" "return" "if" "else" "then" "while" "do" "for"
-    "defer" "break" "continue" "static"
+    "defer" "break" "continue" "static" "tag_union"
     "struct" "enum" "pub" "extern" "using"
     "null" "true" "false" "enum_flag"
     "sizeof" "type_of" "kind" "case" "goto" "label" "default")
